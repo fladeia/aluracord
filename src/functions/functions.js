28846fbda5c5ee617b16ba131ade85fc1780cd15
fladeia <252842.5s@gmail.com>
2022-01-29 10:02:28 -0300

    refac supabase functions

---
+++ src/functions/functions.js
@@ -0,0 +1,22 @@
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_URL = 'https://zvclbplibmpvksrkfwcf.supabase.co'
+const SUPABASE_ANON_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzI4MDM4OCwiZXhwIjoxOTU4ODU2Mzg4fQ.hYLGR67NDdaTdednlqpT3Thywxa08WdANQ5Wzv75AnY'
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+export const SupabaseSelect = async setMessageList => {
+  const { data, error } = await supabaseClient
+    .from('messages')
+    .select()
+    .order('id', { ascending: false })
+  setMessageList(data)
+  console.log(`Error - select supabase:  ${error}`)
+}
+
+export const SupabaseInsert = async message => {
+  const { data, error } = await supabaseClient
+    .from('messages')
+    .insert([message])
+  console.log(`Error - insert supabase:  ${error}`)
+}
